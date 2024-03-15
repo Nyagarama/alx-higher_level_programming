9ef3e12530630378e4881b6809ac868764eef84c
Nyagarama <mochristopher27@gmail.com>
2024-03-15 06:13:45 -0700

    Convert to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,8 @@
+-- Converts the following to UTF8:
+--				1). Database hbtn_0c_0
+--				2). Table first_table
+--				3). Field "name" in the "first_table"
+
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4_unicode_ci;
