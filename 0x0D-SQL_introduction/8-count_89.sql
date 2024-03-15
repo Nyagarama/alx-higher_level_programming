5f8f61b2d744ac82896dcd9259db39d1bff357a1
Nyagarama <mochristopher27@gmail.com>
2024-03-15 03:09:40 -0700

    number of records

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,7 @@
+-- A script that displays the number of records with;
+--						1) id = 89
+--						2) table as first table
+-- Database source is 'hbtn_0c_0' in my MySQL
+-- Database name is passed as an argument of the 'mysql' command
+
+SELECT COUNT(*) as id FROM first_table WHERE id=89;
