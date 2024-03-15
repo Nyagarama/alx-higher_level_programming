db5ff03da1012fd33553f7ce77fee1407b3cc939
Nyagarama <mochristopher27@gmail.com>
2024-03-15 03:03:44 -0700

    Insert Value

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,8 @@
+-- A script that inserts a new row in the table 'first table'
+-- Source of database is 'hbtn_0c_0' in my MySQL server
+-- Details of the New row:
+--			id = 89
+--			name = Best School
+-- Database name will be passed as an argument of the 'mysql' command
+
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School');
