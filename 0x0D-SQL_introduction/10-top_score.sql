bff808c91ee55580734c945c74ba47dad48e95e3
Nyagarama <mochristopher27@gmail.com>
2024-03-15 05:22:15 -0700

    top score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- A script that lists all records of the table `second table`
+-- Database source is `hbtn_0c_0`in my MySQL
+-- Order of display; Score then name
+
+SELECT score, name FROM second_table ORDER BY score DESC;
