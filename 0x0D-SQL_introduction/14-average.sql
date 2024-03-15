b18403a750c207a1faf2bf37634f5259057bfc8d
Nyagarama <mochristopher27@gmail.com>
2024-03-15 05:46:49 -0700

    Average Score

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- A script that computes the score average of all records
+-- Table name: second_table of the database hbtn_0c_0 in my MySQL
+-- The result column name should be average
+
+SELECT AVG(score) AS average FROM second_table;
