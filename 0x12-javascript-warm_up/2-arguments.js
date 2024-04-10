73efce3755796c7196f6aaf5b5aa3336956db5d3
Nyagarama <mochristopher27@gmail.com>
2024-04-10 13:55:15 -0700

    Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-// only the node command and script filename
 if (process.argv.length === 2) {
-  console.log('No Argument');
+  console.log('No argument');
 } else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
